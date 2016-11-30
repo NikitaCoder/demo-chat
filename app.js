a2f1d440e25f54892c13e5bd718ce313c3beedc0
ILYA <roilylitter@gmail.com>
2016-11-30 13:07:16 +0200

    s

--- app.js
+++ app.js
@@ -47,11 +47,11 @@ app.all('*',(req,res)=>{
 
 
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+/*var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'*/
  
-server.listen(server_port, server_ip_address, function () {
-  console.log( "Listening on " + server_ip_address + ", port " + server_port )
+server.listen(process.env.PORT || 3000, function(){
+	console.log("Listening on 127.0.0.1, port " + (process.env.PORT || 3000));
 });
 
 
